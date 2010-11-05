7b83aa05e48d5030e5eac69c67099b4c7223e87b
August Muench <august.fly@gmail.com>
2010-11-05 17:04:07 -0400

    converted dict mashing to functions

--- adsdatalinks/working.py
+++ adsdatalinks/working.py
@@ -2,42 +2,42 @@
 from collections import defaultdict
 from simplejson import load
 
+def countkeys(dictlist,
+              dictkeys = ['bibcode','journal','pubyear']):
+    t = {}
+    keys = []
+    for k in dictkeys:
+        pkey = k+'s'
+        keys.append(pkey)
+        t[pkey] = defaultdict(int)
+
+    for d in dictlist:
+        for x,y in zip(keys,dictkeys):
+            v = d.get(y)
+            t[x][v] += 1
+     
+    return t
+
+def repackdictlist(dictlist,
+                   rkey = None):
+    t = defaultdict(list)
+    rkey = rkey and rkey or dictlist[0].keys()[0]
+
+    for d in dictlist:
+        v = d.get(rkey)
+        t[v].append(d)
+    
+    return t
+        
+
 d = "../../../Data/astrodatacite/ADS/extlinks.json"
 f = open(d,"r")
 data = load(f)
 f.close()
 
-papers = data['rows']
-
-linktypes = set()
-for paper in papers:
-    linktypes.add(paper['link type'])
-
-linkdict = {}
-for linktype in linktypes: 
-    linkdict[linktype] = {'count':0,
-                          'links':[],
-                          'bibcodes':[],
-                          'papers':[]}
-
-for paper in papers:
-    linkdict[paper['link type']]['count'] += 1
-    linkdict[paper['link type']]['bibcodes'].append(paper['bibcode'])
-    #linkdict[paper['link type']]['links'].append(paper['bibcode'])
-    linkdict[paper['link type']]['papers'].append(paper)
+links = data['rows']
+linkdict = repackdictlist(links,rkey='link type')
+linktypes = linkdict.keys()
 
 keys = ['bibcode','journal','pubyear']
-bibcodes = {'datasets':defaultdict(int)}
-pubyears = {'bibcodes':defaultdict(int),'datasets':defaultdict(int)}
-journals = {'bibcodes':defaultdict(int),'datasets':defaultdict(int)}
-
-request = 'DatasetLink'
-for w in linkdict[request]['papers']:
-    b,j,y = (w.get(x) for x in keys)
-    pubyears['datasets'][y] += 1
-    journals['datasets'][j] += 1
-    if b not in bibcodes['datasets'].keys():
-        pubyears['bibcodes'][y] += 1
-        journals['bibcodes'][j] += 1
-    bibcodes['datasets'][b] += 1
 
